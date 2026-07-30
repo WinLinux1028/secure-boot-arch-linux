63eefb7b2a2a00aa76082ac8a0127013d015b3f8
Win‮8201‭Linux‬ <reimussr@gmail.com>
2026-07-31 08:21:03 +0900

    初回インストール時にカーネルが署名されない問題を修正

--- install.sh
+++ install.sh
@@ -18,23 +18,6 @@ if [ ${#INSTALL_PACKAGES[@]} -ne 0 ]; then
     ${AUR_HELPER} "${INSTALL_PACKAGES[@]}"
 fi
 
-./update-grub.sh
-./update-shim.sh
-
-ESP_DIR="$( findmnt -n -o TARGET -T "${EFI_DIR}" )"
-if [ -n "${BOOTENTRY_NAME}" ]; then
-    EXISTING_SAME_NAME_BOOT_ENTRY="$( efibootmgr | grep -P "^Boot[0-9A-Fa-f]+?[^\s]*?\s+?${BOOTENTRY_NAME}" | sed -nE 's/^Boot([0-9A-Fa-f]+).*?$/\1/p' )"
-    for bootnum in ${EXISTING_SAME_NAME_BOOT_ENTRY}; do
-        efibootmgr --bootnum "${bootnum}" --delete-bootnum
-    done
-
-    ESP_PART="$( findmnt -n -o SOURCE -T "${EFI_DIR}" )"
-    DISK="/dev/$( lsblk -nd -o PKNAME "${ESP_PART}" )"
-    ESP_PART_NUM="$( lsblk -nd -o PARTN "${ESP_PART}" )"
-    RELATIVE_EFI_DIR="${EFI_DIR#${ESP_DIR}}"
-    efibootmgr --unicode --create --label "${BOOTENTRY_NAME}" --disk "${DISK}" --part "${ESP_PART_NUM}" --loader "${RELATIVE_EFI_DIR}/BOOTX64.EFI"
-fi
-
 rm -rf pacman-hooks
 for script in pacman-hooks.gen/*.sh; do
     "${script}"
@@ -56,7 +39,24 @@ done
 mkdir -p /etc/initcpio/post
 ln -si "$( pwd )/mkinitcpio-hooks"/* /etc/initcpio/post/
 
+mkinitcpio -P
+./update-grub.sh
+./update-shim.sh
 cp -f mok/mok.cer "${ESP_DIR}/"
+
+ESP_DIR="$( findmnt -n -o TARGET -T "${EFI_DIR}" )"
+if [ -n "${BOOTENTRY_NAME}" ]; then
+    EXISTING_SAME_NAME_BOOT_ENTRY="$( efibootmgr | grep -P "^Boot[0-9A-Fa-f]+?[^\s]*?\s+?${BOOTENTRY_NAME}" | sed -nE 's/^Boot([0-9A-Fa-f]+).*?$/\1/p' )"
+    for bootnum in ${EXISTING_SAME_NAME_BOOT_ENTRY}; do
+        efibootmgr --bootnum "${bootnum}" --delete-bootnum
+    done
+
+    ESP_PART="$( findmnt -n -o SOURCE -T "${EFI_DIR}" )"
+    DISK="/dev/$( lsblk -nd -o PKNAME "${ESP_PART}" )"
+    ESP_PART_NUM="$( lsblk -nd -o PARTN "${ESP_PART}" )"
+    RELATIVE_EFI_DIR="${EFI_DIR#${ESP_DIR}}"
+    efibootmgr --unicode --create --label "${BOOTENTRY_NAME}" --disk "${DISK}" --part "${ESP_PART_NUM}" --loader "${RELATIVE_EFI_DIR}/BOOTX64.EFI"
+fi
 
 echo "✅️ Installation successful!"
 echo "MokManager will launch during the first boot in Secure Boot mode, so please enroll the mok.csr file located at the root of the ESP."
